c04bc576c1bde0b14d5cfdd0d80927645ed2161f
Henry Stanley <henry@henrystanley.com>
2014-10-03 12:19:33 +0100

    Generated registrations controller, updated home page signin/out links
    Registrations controller is a custom version of Devise's built-in
    controller and was altered to permit a user to register with a username.

--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -16,12 +16,47 @@ feature "Homepages", :type => :feature do
 
     it 'should see sign up and sign in links' do
       expect(page).to have_link("Sign up")
-      expect(page).to have_link("Sign in")
+      expect(page).to have_link("Log in")
+    end
+
+  end
+
+  context 'signing in users' do
+
+    before(:each) do
+      visit '/'
     end
 
     it 'clicking the sign up link should take a user to the sign up page' do
       click_link("Sign up")
       expect(page).to have_content("Sign up")
+    end
+
+    it 'clicking the sign in link should take a user to the sign in page' do
+      click_link("Log in")
+      expect(page).to have_content("Log in")
+    end
+
+    it 'once a user signs in they should see "Welcome back, name"' do
+      click_link("Sign up")
+      fill_in("Username", :with => "henryaj")
+      fill_in("Email", :with => "test@example.com")
+      fill_in("Password", :with => "12345678")
+      fill_in("Password confirmation", :with => "12345678")
+      click_button("Sign up")
+      expect(page).to have_content("You have signed up successfully")
+    end
+
+    it 'if a user is signed in, they should see a sign out link and not log in or sign up links' do
+      click_link("Sign up")
+      fill_in("Username", :with => "henryaj")
+      fill_in("Email", :with => "test@example.com")
+      fill_in("Password", :with => "12345678")
+      fill_in("Password confirmation", :with => "12345678")
+      click_button("Sign up")
+      expect(page).to have_content("Sign out")
+      expect(page).not_to have_content("Sign up")
+      expect(page).not_to have_content("Log in")
     end
 
   end
