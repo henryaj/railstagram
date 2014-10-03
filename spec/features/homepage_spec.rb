74778e258154dcd9d4a73908ee4e77b2d39cd0a6
Henry Stanley <henry@henrystanley.com>
2014-10-03 11:31:34 +0100

    Added users controller, created user#new page with title

--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 feature "Homepages", :type => :feature do
-  
+
   context 'a visitor on the homepage' do
 
     before do
@@ -17,6 +17,11 @@ feature "Homepages", :type => :feature do
     it 'should see sign up and sign in links' do
       expect(page).to have_link("Sign up")
       expect(page).to have_link("Sign in")
+    end
+
+    it 'clicking the sign up link should take a user to the sign up page' do
+      click_link("Sign up")
+      expect(page).to have_content("Sign up for Railstagram")
     end
 
   end
