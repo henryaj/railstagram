52be4c52336e06672cddd22bc76c9d7353b0d4e8
Henry Stanley <henry@henrystanley.com>
2014-10-04 23:55:41 +0100

    Added post image button to homepage, linked users and posts through a has_many relationship

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141003144923) do
+ActiveRecord::Schema.define(version: 20141003214621) do
 
   create_table "posts", force: true do |t|
     t.datetime "created_at"
@@ -34,9 +34,11 @@ ActiveRecord::Schema.define(version: 20141003144923) do
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
     t.string   "username"
+    t.integer  "post_id"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["post_id"], name: "index_users_on_post_id"
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
--- spec/features/creating_posts_spec.rb
+++ spec/features/creating_posts_spec.rb
@@ -26,9 +26,16 @@ feature "Creating posts", :type => :feature do
       expect(page).to have_css('form')
     end
 
-    xit 'a visitor can fill in the form and submit an image, after which they should see "image posted"' do
+    xit 'a visitor can fill in the form and submit an image, after which they should see "image posted" and their username on the homepage' do
       Capybara.current_driver = :selenium
       visit '/'
+      click_link("Sign up")
+      fill_in("Username", :with => "henry")
+      fill_in("Email", :with => "test@test.com")
+      fill_in("Password", :with => "12345678")
+      fill_in("Password confirmation", :with => "12345678")
+      click_button("Sign up")
+      sleep 2
       click_link("Log in")
       fill_in("Email", :with => "test@test.com")
       fill_in("Password", :with => "12345678")
@@ -36,19 +43,17 @@ feature "Creating posts", :type => :feature do
       sleep 3
       click_link 'Post image'
       fill_in("Caption", :with => "Beautiful day")
-
-      # this click_on will trigger filepicker.io plugin
       click_on 'Upload'
-      # waiting for filepicker.io
       sleep 5
       within_frame 'filepicker_dialog' do
-        # attach image to filepicker file input and upload...
         find('#fileUploadInput', visible: false).set("#{Rails.root}/spec/features/image.jpg")
         sleep 5
       end
-
       click_button("Submit")
       expect(page).to have_content("Photo uploaded.")
+      expect(page).to have_content("Beautiful day")
+      expect(page).to have_content("henryaj")
+      expect(page).to have_css("img")
     end
 
   end
