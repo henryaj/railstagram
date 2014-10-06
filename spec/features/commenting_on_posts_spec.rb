bc62d28bd0064438792887b2d608bf876b61bbbb
Henry Stanley <henry@henrystanley.com>
2014-10-06 15:55:25 +0100

    extracted views into partials

--- spec/features/commenting_on_posts_spec.rb
+++ spec/features/commenting_on_posts_spec.rb
@@ -4,16 +4,17 @@ feature "Commenting on posts", :type => :feature do
 
   before do
       @post = create(:post1)
-      visit '/'
   end
 
   it 'when a user clicks on a photo, they should see a modal with a comment box and a submit button' do
+    visit '/'
     page.find('#modal1').click
     expect(page).to have_css('input')
     expect(page).to have_css('button')
   end
 
   it 'when a user clicks on a photo, they should see a modal with a comment box and a submit button' do
+    visit '/'
     page.find('#modal1').click
     fill_in('input', with: "great photo")
     click_button("Post comment")
